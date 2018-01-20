f58d6485e20e5bb0f2ada17b0fa773fa4ef5a243
Nathaniel <32886769+KNO3CS@users.noreply.github.com>
2018-01-20 12:24:11 -0700

    added encoder

--- 2018Stacys_Mom/src/org/usfirst/frc/team3070/robot/Drive.java
+++ 2018Stacys_Mom/src/org/usfirst/frc/team3070/robot/Drive.java
@@ -6,48 +6,55 @@ import edu.wpi.first.wpilibj.Encoder;
 public class Drive {
 	
 TalonSRX TalRM, TalRF, TalLM, TalLF;
+Encoder encR,encL;
 int PORT_RM, PORT_RF,PORT_LM,PORT_LF;
-	public Drive(int rm,int rf, int lm, int lf) {
+	public Drive(int rm,int rf, int lm, int lf, int encoderR1, int encoderR2, int encoderL1, int encoderL2) {
 		TalRM = new TalonSRX(rm); //Right master Talon
 		TalRF = new TalonSRX(rf); //Right follower Talon
 		TalLM = new TalonSRX(lm); //Left master Talon
 		TalLF = new TalonSRX(lf); //Left follower Talon
+		encR = new Encoder(encoderR1, encoderR2, false); //Right encoder
+		encL = new Encoder(encoderL1, encoderL2, false); //Left encoder
 	}
+	//open variables:
 	 /**
 	  * Sets right side motors to a certain amount, given by arg
 	  */
-	boolean noMovement;
+
 	void setRight(double amount) {
-		if(noMovement==false) {
+
 		TalRM.set(ControlMode.PercentOutput, amount);
 		TalRF.set(ControlMode.Follower, PORT_RM);
 	}
-	}
+	
 	/**
 	  * Sets left side motors to a certain amount, given by arg
 	  */
 	void setLeft(double amount) {
-		if (noMovement==false) {
+	
 		TalLM.set(ControlMode.PercentOutput, amount);
 		TalLF.set(ControlMode.Follower, PORT_LF);
 	}
-	}
+	
 	void stop(boolean stopping) {
 		if(stopping==true) {
 		setRight(0);
 		setLeft(0);
-		noMovement = true;
-	}else {
-		noMovement = false;
+		}
 	}
 		
-	}
-	void move(double moving) {
-		if(noMovement==false) {
-		TalLM.set(ControlMode.PercentOutput, moving);
-		TalLF.set(ControlMode.Follower, PORT_LF);
-		TalRM.set(ControlMode.PercentOutput, moving);
-		TalRF.set(ControlMode.Follower, PORT_RM);
+	void move(double moving,int rotations) {
+
+		if (encR.getDistance() < rotations && encL.getDistance() < rotations) {
+			setRight(moving);
+			setLeft(moving);
+		}else {
+			encR.reset();
+			encL.reset();
+			setRight(0);
+			setLeft(0);
+			
 		}
+		
 	}
 }
