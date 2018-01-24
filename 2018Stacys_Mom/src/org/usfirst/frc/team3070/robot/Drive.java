3e7ac04c0f7fb5a32458c67c7be624f450e10754
beechnutkjr <xseggs@gmail.com>
2018-01-24 15:48:28 -0800

    cleaning up drive class

--- 2018Stacys_Mom/src/org/usfirst/frc/team3070/robot/Drive.java
+++ 2018Stacys_Mom/src/org/usfirst/frc/team3070/robot/Drive.java
@@ -9,83 +9,82 @@ import org.usfirst.frc.team3070.robot.Robot;
 public class Drive {
 	
 	@SuppressWarnings("unused")
-Encoder encR,encL;
 
-	public Drive() {
+public Drive() {
 		Pronstants pronstants;
-	void initialize() {
-		Talon SRX TalRM = new TalonSRX(pronstants.PORT_RM); //Right master Talon
-		Talon SRX TalRF = new TalonSRX(pronstants.PORT_RF); //Right follower Talon
-		TalonSRX TalLM = new TalonSRX(pronstants.PORT_LM); //Left master Talon
-		Talon SRXTalLF = new TalonSRX(pronstants.PORT_LF); //Left follower Talon
-		Encoder encR = new Encoder(pronstants.PORT_ENC_R1, pronstants.PORT_ENC_R2, false); //Right encoder
-		Encoder encL = new Encoder(pronstants.PORT_ENC_L1, pronstants.PORT_ENC_L2, false); //Left encoder
-	}
-	//open variables:
-	 /**
-	  * Sets right side motors to a certain amount, given by arg
-	  */
-
-	void setRight(double amountR) {
-		TalRM.set(ControlMode.PercentOutput, amountR);
-		TalRF.set(ControlMode.Follower, PORT_RF);
-	}
+		void initialize() {
+			Talon SRX TalRM = new TalonSRX(pronstants.PORT_RM); //Right master Talon
+			Talon SRX TalRF = new TalonSRX(pronstants.PORT_RF); //Right follower Talon
+			TalonSRX TalLM = new TalonSRX(pronstants.PORT_LM); //Left master Talon
+			Talon SRXTalLF = new TalonSRX(pronstants.PORT_LF); //Left follower Talon
+			Encoder encR = new Encoder(pronstants.PORT_ENC_R1, pronstants.PORT_ENC_R2, false); //Right encoder
+			Encoder encL = new Encoder(pronstants.PORT_ENC_L1, pronstants.PORT_ENC_L2, false); //Left encoder
+		}
+		//open variables:
+		 /**
+		  * Sets right side motors to a certain amount, given by arg
+		  */
 	
-	/**
-	  * Sets left side motors to a certain amount, given by arg
-	  */
-	void setLeft(double amountL) {
-		TalLM.set(ControlMode.PercentOutput, amountL);
-		TalLF.set(ControlMode.Follower, PORT_LF);
-	}
-	void stop(boolean stopping) {
-		setRight(0);
-		setLeft(0);
-	}
-		
-
-	/**
-	 * Constructor for Drive
-	 * @param rm right master
-	 * @param rf right follower
-	 * @param lm left master
-	 * @param lf left follower
-	 */
-	public Drive(int rm,int rf, int lm, int lf) {
-		TalRM = new TalonSRX(rm); //Right master Talon
-		TalRF = new TalonSRX(rf); //Right follower Talon
-		TalLM = new TalonSRX(lm); //Left master Talon
-		TalLF = new TalonSRX(lf); //Left follower Talon
-	}
-	
-	/**
-	 * Sets right side motors to a certain value
-	 * @param amount speed for motors
-	 *
-	
-	
-	/**
-	 * Sets both sides to a certain value
-	 * @param amount speed for motors
-	 */
-	void move(double amount) {
-		setRight(amount);
-		setLeft(amount);
-	}
-
-	void move(double moving, int rotations) {
-
-		if (encR.getDistance() < rotations && encL.getDistance() < rotations) {
-			setRight(moving);
-			setLeft(moving);
-		}else {
-			encR.reset();
-			encL.reset();
-			stopL(true);
-			stopR(true);
-			
+		void setRight(double amountR) {
+			TalRM.set(ControlMode.PercentOutput, amountR);
+			TalRF.set(ControlMode.Follower, PORT_RF);
 		}
 		
-	}
+		/**
+		  * Sets left side motors to a certain amount, given by arg
+		  */
+		void setLeft(double amountL) {
+			TalLM.set(ControlMode.PercentOutput, amountL);
+			TalLF.set(ControlMode.Follower, PORT_LF);
+		}
+		void stop(boolean stopping) {
+			setRight(0);
+			setLeft(0);
+		}
+			
+	
+		/**
+		 * Constructor for Drive
+		 * @param rm right master
+		 * @param rf right follower
+		 * @param lm left master
+		 * @param lf left follower
+		 */
+		public Drive(int rm,int rf, int lm, int lf) {
+			TalRM = new TalonSRX(rm); //Right master Talon
+			TalRF = new TalonSRX(rf); //Right follower Talon
+			TalLM = new TalonSRX(lm); //Left master Talon
+			TalLF = new TalonSRX(lf); //Left follower Talon
+		}
+		
+		/**
+		 * Sets right side motors to a certain value
+		 * @param amount speed for motors
+		 *
+		
+		
+		/**
+		 * Sets both sides to a certain value
+		 * @param amount speed for motors
+		 */
+		void move(double amount) {
+			setRight(amount);
+			setLeft(amount);
+		}
+	
+		void move(double moving, int rotations) {
+	
+			if (encR.getDistance() < rotations && encL.getDistance() < rotations) {
+				setRight(moving);
+				setLeft(moving);
+			}else {
+				encR.reset();
+				encL.reset();
+				stopL(true);
+				stopR(true);
+				
+			}
+			
+		}
 
 }
