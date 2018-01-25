87f655f14bd918e63d22ed76f5601fb0826f6d2a
Nathaniel <32886769+KNO3CS@users.noreply.github.com>
2018-01-24 17:25:13 -0700

    XAIVIe-*cough cough*

--- 2018Stacys_Mom/src/org/usfirst/frc/team3070/robot/Sensors.java
+++ 2018Stacys_Mom/src/org/usfirst/frc/team3070/robot/Sensors.java
@@ -1,4 +1,23 @@
 
-public class Sensors {
+package org.usfirst.frc.team3070.robot;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.Encoder;
+
+public class Sensors implements Pronstants {
+	Encoder encL, encR;
+	AnalogGyro gyro;
+	
+	/**
+	 * Constructor for sensors
+	 */
+	public Sensors() {
+		//Initializing encoders
+		encR = new Encoder(PORT_ENC_R1, PORT_ENC_R2, false); //Right encoder
+		encL = new Encoder(PORT_ENC_L1, PORT_ENC_L2, false); //Left encoder
+
+		//Initializing Gyros
+		gyro = new AnalogGyro(PORT_GYRO); 
+	}
 }
+
