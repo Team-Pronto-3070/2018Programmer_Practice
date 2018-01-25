66ebc4ed06a4494d577279d6e0aa0edd63d46ba2
beechnutkjr <xseggs@gmail.com>
2018-01-25 13:37:57 -0800

    drive cleanup/garbage collection/redudancy reduction

--- 2018Stacys_Mom/src/org/usfirst/frc/team3070/robot/Drive.java
+++ 2018Stacys_Mom/src/org/usfirst/frc/team3070/robot/Drive.java
@@ -8,44 +8,12 @@ import org.usfirst.frc.team3070.robot.Robot;
 
 public class Drive {
 
-	@SuppressWarnings("unused")
-<<<<<<< HEAD
-=======
-	TalonSRX TalRM, TalRF, TalLM, TalLF;
-	Encoder encR, encL;
-	int PORT_RM, PORT_RF, PORT_LM, PORT_LF;
-
-	public Drive(int rm,int rf, int lm, int lf, int encoderR1, int encoderR2, int encoderL1, int encoderL2) {
-		TalRM = new TalonSRX(2); //Right master Talon
-		TalRF = new TalonSRX(5); //Right follower Talon
-		TalLM = new TalonSRX(0); //Left master Talon
-		TalLF = new TalonSRX(1); //Left follower Talon
-		encR = new Encoder(encoderR1, encoderR2, false); //Right encoder
-		encL = new Encoder(encoderL1, encoderL2, false); //Left encoder
-	Robot robot = new Robot();
-final int PORT_RM = robot.PORT_RM;
-final int PORT_RF = robot.PORT_RF;
-final int PORT_LM = robot.PORT_LM;
-final int PORT_LF = robot.PORT_LF;
-	}
->>>>>>> it-me-the-stego
-
-public Drive() {
-		Pronstants pronstants;
-		public Drive(int rm, int rf, int lm, int lf) {
-			TalRM = new TalonSRX(rm); // Right master Talon
-			TalRF = new TalonSRX(rf); // Right follower Talon
-			TalLM = new TalonSRX(lm); // Left master Talon
-			TalLF = new TalonSRX(lf); // Left follower Talon
-		}
-	void initialize() {
-			Talon SRX TalRM = new TalonSRX(pronstants.PORT_RM); //Right master Talon
-			Talon SRX TalRF = new TalonSRX(pronstants.PORT_RF); //Right follower Talon
-			TalonSRX TalLM = new TalonSRX(pronstants.PORT_LM); //Left master Talon
-			Talon SRXTalLF = new TalonSRX(pronstants.PORT_LF); //Left follower Talon
-			Encoder encR = new Encoder(pronstants.PORT_ENC_R1, pronstants.PORT_ENC_R2, false); //Right encoder
-			Encoder encL = new Encoder(pronstants.PORT_ENC_L1, pronstants.PORT_ENC_L2, false); //Left encoder
-		}
+	Talon SRX TalRM=new TalonSRX(pronstants.PORT_RM); // Right master Talon
+	Talon SRX TalRF=new TalonSRX(pronstants.PORT_RF); // Right follower Talon
+	TalonSRX TalLM = new TalonSRX(pronstants.PORT_LM); // Left master Talon
+	Talon SRXTalLF = new TalonSRX(pronstants.PORT_LF); // Left follower Talon
+	Encoder encR = new Encoder(pronstants.PORT_ENC_R1, pronstants.PORT_ENC_R2, false); // Right encoder
+	Encoder encL = new Encoder(pronstants.PORT_ENC_L1, pronstants.PORT_ENC_L2, false); // Left encoder
 
 	// open variables:
 	/**
@@ -84,27 +52,6 @@ public Drive() {
 	 * @param lf
 	 *            left follower
 	 */
-	
-
-
-	/**
-	 * Constructor for Drive
-	 * 
-	 * @param rm
-	 *            right master
-	 * @param rf
-	 *            right follower
-	 * @param lm
-	 *            left master
-	 * @param lf
-	 *            left follower
-	 */
-	public Drive(int rm, int rf, int lm, int lf) {
-		TalRM = new TalonSRX(rm); // Right master Talon
-		TalRF = new TalonSRX(rf); // Right follower Talon
-		TalLM = new TalonSRX(lm); // Left master Talon
-		TalLF = new TalonSRX(lf); // Left follower Talon
-	}
 
 	/**
 	 * Sets right side motors to a certain value
