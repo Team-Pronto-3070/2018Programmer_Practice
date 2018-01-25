8ae1ea1cd459da904d1122ed60267fc09acbb7f9
Nathaniel <32886769+KNO3CS@users.noreply.github.com>
2018-01-24 17:44:35 -0700

    small changes

--- 2018Stacys_Mom/src/org/usfirst/frc/team3070/robot/Sensors.java
+++ 2018Stacys_Mom/src/org/usfirst/frc/team3070/robot/Sensors.java
@@ -7,17 +7,16 @@ import edu.wpi.first.wpilibj.Encoder;
 public class Sensors implements Pronstants {
 	Encoder encL, encR;
 	AnalogGyro gyro;
-	
+
 	/**
 	 * Constructor for sensors
 	 */
 	public Sensors() {
-		//Initializing encoders
-		encR = new Encoder(PORT_ENC_R1, PORT_ENC_R2, false); //Right encoder
-		encL = new Encoder(PORT_ENC_L1, PORT_ENC_L2, false); //Left encoder
+		// Initializing encoders
+		encR = new Encoder(PORT_ENC_R1, PORT_ENC_R2, false); // Right encoder
+		encL = new Encoder(PORT_ENC_L1, PORT_ENC_L2, false); // Left encoder
 
-		//Initializing Gyros
-		gyro = new AnalogGyro(PORT_GYRO); 
+		// Initializing Gyros
+		gyro = new AnalogGyro(PORT_GYRO);
 	}
 }
-
