ce832041c1cbd5867ba4a9941f7e69799439bfe3
Wisestgut <rs0977624@k12.shorelineschools.org>
2018-01-27 11:00:35 -0800

    grabber is pretty much done????

--- SM_sucks/src/org/usfirst/frc/team3070/robot/Grabber_for_Pronto.java
+++ SM_sucks/src/org/usfirst/frc/team3070/robot/Grabber_for_Pronto.java
@@ -1,21 +1,40 @@
 package org.usfirst.frc.team3070.robot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 
 public class Grabber_for_Pronto {
 	TalonSRX Lgrab, Rgrab;
 	DigitalInput LimitSwitch;
+	Sensors sensors;
 	
-	public void Grabber_For_Pronto() {
+	public void setGrabs(double power) {
+		Lgrab.set(ControlMode.PercentOutput, power);
+		Rgrab.set(ControlMode.PercentOutput, power);
+	
+	}
+	
+		public void Grabber_For_Pronto() {
 		Lgrab = new TalonSRX(Pronstants.PORT_LARM);
 		Rgrab = new TalonSRX(Pronstants.PORT_RARM);
 		LimitSwitch = new DigitalInput(0);
-		//int amount_of_genders = 2;
 		
+			
 	}
-	if(sensors.joyL.getRawButton(2)) {
-		Lgrab.set(drive.ControlMode.PercentOutput, .6);
+	if(sensors.joyL.getRawButton(1)) {
+		setGrabs(.5);
 	}
+	if(LimitSwitch.get() == 1) {
+		setGrabs(0);
+	}
+	if(sensors.joyL.getRawButton(4)) {
+		Lgrab.set(ControlMode.PercentOutput, .3);
+	}
+	if(sensors.joyR.getRawButtom(5)) {
+		Rgrab.set(ControlMode.PercentOutput, .3);
+	}
+	
 }
--- SM_sucks/src/org/usfirst/frc/team3070/robot/Pronstants.java
+++ SM_sucks/src/org/usfirst/frc/team3070/robot/Pronstants.java
@@ -22,7 +22,7 @@ public interface Pronstants {
 	final double WEAK_SPEED = -.5;
 	final double STRONG_SPEED = .5;
 	final int PORT_GYRO = 9;
-	
+	final int amount_of_genders = 2; //As the wise kyle once said...
 	public enum Auto_Path { // List of all possible paths (PATH_[Left, Center, or Right starting
 						   // postition][sCale or sWitch][Right or Left Side])
 		PATH_LCL, // Left starting position combinations
